332b3b62f8a9f81bf6d8c9b809f9460eb3b933c8
WiseDataApps <108593838+WiseDataApps@users.noreply.github.com>
2022-07-03 09:02:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,11 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Favorites")
 
-streamlit.text(🥣 "Omega 3 and Blueberry Oatmeal")
+streamlit.text("🥣 Omega 3 and Blueberry Oatmeal")
 
-streamlit.text(🥗"Kale, Spinach & Rocket Smoothie")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 
-streamlit.text(🐔"Hard-Boiled Free-Range Egg")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 
-streamlit.text( 🥑🍞"Avacado Toast")
+streamlit.text("🥑🍞 Avacado Toast")
  

18d92f4f216088efa84f34d6199e5b67d1ef1cdf
WiseDataApps <108593838+WiseDataApps@users.noreply.github.com>
2022-07-05 08:44:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,7 @@ streamlit.dataframe(fruits_to_show)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+# write your own comment - this usese a pandas method to normalize the json response from the api
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - this is a streamlit method that renders a dataframe as a table in a webpage.
+streamlit.dataframe(fruityvice_normalized)

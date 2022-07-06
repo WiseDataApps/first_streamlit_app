a4868a1ed03df857a1f528b58fbe1893b7243103
WiseDataApps <108593838+WiseDataApps@users.noreply.github.com>
2022-07-06 09:41:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,8 +54,13 @@ if streamlit.button('Get Fruit Load List'):
  my_data_rows = get_fruit_load_list()
  streamlit.dataframe(my_data_rows)
 
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+   my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+   return 'Thanks for adding ' + add_my_fruit
+   
+if streamlit.button("Add a Fruit to the List"):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake()
+  streamlit.text(back_from_function)
 
-streamlit.stop()
-fruit_addition = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ', fruit_addition)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")

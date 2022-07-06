7e21ea666aaf2eafcb0f816b91ee8feaaa0c5f86
WiseDataApps <108593838+WiseDataApps@users.noreply.github.com>
2022-07-06 09:30:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,11 @@ fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+ return fruityvice_normalized
+ 
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -33,11 +38,8 @@ try:
  if not fruit_choice:
   streamlit.error('Please select a fruit to get information.')
  else:
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-   # write your own comment - this usese a pandas method to normalize the json response from the api
-  fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-   # write your own comment - this is a streamlit method that renders a dataframe as a table in a webpage.
-  streamlit.dataframe(fruityvice_normalized)
+  back_from_function = get_fruityvice_data(fruit_choice)
+  streamlit.dataframe(back_from_function)
 except URLError as e:
  streamlit.error()
    
